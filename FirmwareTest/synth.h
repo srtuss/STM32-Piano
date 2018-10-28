7e5b8f6db1170166276919e5d698687b51b0c510
srtuss <stral@aon.at>
2018-10-28 21:15:03 +0100

    synth module start

---
+++ FirmwareTest/synth.h
@@ -0,0 +1,21 @@
+#ifndef _SYNTH_H_
+#define _SYNTH_H_
+
+#include "voice.h"
+#include "reverb.h"
+
+typedef struct synth_t
+{
+	voice_s voice;
+	reverb_s reverb;
+} synth_s;
+
+typedef struct synth_stereo_t
+{
+	float left, right;
+} synth_stereo_s;
+
+extern void synth_init(synth_s *s);
+extern synth_stereo_s synth_clock(synth_s *s);
+
+#endif
